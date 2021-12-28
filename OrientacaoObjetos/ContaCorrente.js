0c5793048ef3e2909812b982435c26a14ab086f0
Romero Jose Ferreira <romero@bb.com.br>
2021-12-28 11:58:31 -0300

    Conclusão Orientação Objetos

--- OrientacaoObjetos/ContaCorrente.js
+++ OrientacaoObjetos/ContaCorrente.js
@@ -1,24 +1,46 @@
-export class ContaCorrente{
+import { Cliente } from "./Cliente.js";
+
+export class ContaCorrente {
+    static numeroDeContas = 0;
     agencia;
-    cliente;
-    _saldo = 0;
+    #cliente;
+    #saldo = 0;
+
+    constructor(cliente, agencia) {
+        this.#cliente = cliente;
+        this.agencia = agencia;
+        ContaCorrente.numeroDeContas += 1;
+    }
+
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this.#cliente = novoValor;
+        }
+    }
+
+    get cliente() {
+        return this.#cliente;
+    }
+
+    get saldo() {
+        return this.#saldo;
+    }
 
     sacar(valor) {
-        if(this._saldo >= valor) {
-            this._saldo -= valor;
+        if (this.#saldo >= valor) {
+            this.#saldo -= valor;
             return valor;
         }
     }
 
-    depositar(valor){
-        if(valor <=0)
-        {
+    depositar(valor) {
+        if (valor <= 0) {
             return;
         }
-        this._saldo += valor;
+        this.#saldo += valor;
     }
 
-    transferir(valor, conta){
+    transferir(valor, conta) {
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
     }
